d38c34367715e7a69b2af275e92d311ac508161f
unknown <jonathanaveledo@gmail.com>
2019-01-06 18:16:35 -0400

    Integrada la base de datos al proyecto, guardando las ordenes en la base de datos

--- introvenv/sitiopizzeria/homepage/models.py
+++ introvenv/sitiopizzeria/homepage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Orden(models.Model):
+	orden_detalle = models.CharField(max_length=500)
+	precio = models.CharField(max_length=500)
+	fecha = models.CharField(max_length=500)
