5a56930c18663267fa802fb835e958acf0babc4f
Marcolicini <46036581+Marcolicini@users.noreply.github.com>
2019-01-08 22:08:19 -0400

    Docs BD

--- introvenv/sitiopizzeria/homepage/models.py
+++ introvenv/sitiopizzeria/homepage/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 """Acá se define la base de datos que se diseñó para almacenar la data de los pedidos, tomando en cuenta
-las consultas que se debían mostrar en la sección de Administración."""
+las consultas que se debían mostrar en la sección de Administración.
+Se decidió crear una tabla con tres campos: La orden completa de la factura, esto incluye todos los valores 
+y los detalles de la orden. El precio total de cada factura, y la fecha en la que se registraron."""
 # Create your models here.
 class Orden(models.Model):
 	orden_detalle = models.CharField(max_length=500)
