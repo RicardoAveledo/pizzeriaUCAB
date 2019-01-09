276182e2d51450fcd92a5f0dc674c14c8bc082cb
Marcolicini <46036581+Marcolicini@users.noreply.github.com>
2019-01-08 22:07:03 -0400

    Doc Models.py Definiendo Orden

--- introvenv/sitiopizzeria/homepage/models.py
+++ introvenv/sitiopizzeria/homepage/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+"""Acá se define la base de datos que se diseñó para almacenar la data de los pedidos, tomando en cuenta
+las consultas que se debían mostrar en la sección de Administración."""
 # Create your models here.
 class Orden(models.Model):
 	orden_detalle = models.CharField(max_length=500)
